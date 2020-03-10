e0bf679fae6808cf4cee8b7276244cb75177b7e1
jdzgd <jiangdongzhao@foxmail.com>
2020-03-10 16:13:19 +0800

    feat: mock

---
+++ solr/mock/input.sh
@@ -0,0 +1,7 @@
+  curl -X POST -H 'Content-Type: application/json' 'http://localhost:8983/solr/gettingstarted/update?commit=true' --data-binary '
+[{
+    "id": "d11sf",
+    "title": [
+      "sdg"
+    ]
+}]'
